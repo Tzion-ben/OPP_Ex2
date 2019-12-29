8dcac01a453544a5e1f0c7de79959e2812a09e36
Tzion_ben <tzion14@gmail.com>
2019-12-29 02:15:02 +0200

    finising the isConnected method

--- src/algorithms/Graph_Algo.java
+++ src/algorithms/Graph_Algo.java
@@ -78,84 +78,41 @@ public class Graph_Algo implements graph_algorithms{
 		// TODO Auto-generated method stub
 
 	}
-
+	/**
+	 * this method is checking if the directional graph is connect or not by
+	 * the method of DPS traversal on the graph recursivly
+	 */
 	@Override
 	public boolean isConnected() {
 		Collection<node_data> vertices= this.grafAlgo.getV();
 		Iterator<node_data> vert=vertices.iterator();
 		while(vert.hasNext()) {
 			node_data tempVertex=new NodeData(vert.next().getKey());			
-			DFSRec(tempVertex);	
+			DFSRec(tempVertex,this.grafAlgo);	
 		}
+
 		Iterator<node_data> vertSec=vertices.iterator();
 		while(vertSec.hasNext()) {
-			if(vertSec.next().getTag()==0) {
+			if(vertSec.next().getTag()==0) 
 				return false;
-			}
 		}
-				
+
 		graph transpoGraph=transpozeGraph();
 		Collection<node_data> verticesT= transpoGraph.getV();
 		Iterator<node_data> vertT=verticesT.iterator();
 		while(vertT.hasNext()) {
 			node_data tempVertexT=new NodeData(vertT.next().getKey());			
-			//DFSRec(tempVertexT);	
+			DFSRec(tempVertexT ,transpoGraph);	
 		}
 		Iterator<node_data> vertSecT=verticesT.iterator();
 		while(vertSecT.hasNext()) {
-			if(vertSecT.next().getTag()==0) {
+			if(vertSecT.next().getTag()==0) 
 				return false;
-			}
 		}
 		return true;
 	}
 
-	/**
-	 * this methos is run on every neighbors of the node by the dest id of all the edges of the 
-	 * specific node, and tag all the neighbors with '1' that mean this node was visited
-	 * so at the end if all the nodes tags will be 1 the graph is strongly connected  
-	 * tag==0 :NOT VISETED ,tag==1 :VISETED
-	 * @param ver
-	 */
-	public void DFSRec(node_data vert) {
-		vert.setTag(1);
-		Collection<edge_data> edgesOfVert= this.grafAlgo.getE(vert.getKey());
-		Iterator<edge_data> edge=edgesOfVert.iterator();
-		while(edge.hasNext()) {
-			node_data tempVert=this.grafAlgo.getNode(edge.next().getDest());
-			//the next node to check is the vertex that is near we know it by the DEST
-			if(tempVert.getTag()==0)
-				DFSRec(tempVert);
-		}
-	}//end DFSRec
-
-	/**
-	 * return the transpose graph the this graph
-	 * @return
-	 */
-	public graph transpozeGraph () {
-		graph grafAlgoT=new DGraph();
-		Collection<node_data> vertices= this.grafAlgo.getV();
-		Iterator<node_data> vert=vertices.iterator();
-		while(vert.hasNext()) {
-			node_data tempVertex=new NodeData(vert.next().getKey());
-			tempVertex.setTag(0);//sets all the tags to 0 so can run again on the transpose graph
-			grafAlgoT.addNode(tempVertex);	
-		}	//create the Vertex, for dint stop at NULLPOINTEREXPTION with the oppisid srd and dest
-		
-		Iterator<node_data> vert1=vertices.iterator();
-		while(vert1.hasNext()) {
-			node_data tempVertex=new NodeData(vert1.next().getKey());
-			//create the edges of the every vertex
-			Collection<edge_data> edgesOfVert= this.grafAlgo.getE(tempVertex.getKey());
-			Iterator<edge_data> edge=edgesOfVert.iterator();
-			while(edge.hasNext()) {
-				edge_data tempEdge=edge.next();
-				grafAlgoT.connect(tempEdge.getDest(),tempEdge.getSrc(), tempEdge.getWeight());			
-			}		
-		}//create all the transpose graph
-		return grafAlgoT;
-	}
+	
 
 	@Override
 	public double shortestPathDist(int src, int dest) {
@@ -199,19 +156,54 @@ public class Graph_Algo implements graph_algorithms{
 		return graphToCopy;
 	}
 	//****************** My methods *****************
+	
 	/**
-	 * this method checking if two graps_Algo are equal
-
-	@Override
-	public boolean equals(Object obj) {
-		if(obj!=null&&(obj instanceof graph_algorithms)){
-			Graph_Algo n=(Graph_Algo) obj;
-			if(!this.grafAlgo.)
-				return false;
-		}
-		return true;
-	}//end equal
+	 * this methos is run on every neighbors of the node by the dest id of all the edges of the 
+	 * specific node, and tag all the neighbors with '1' that mean this node was visited
+	 * so at the end if all the nodes tags will be 1 the graph is strongly connected  
+	 * tag==0 :NOT VISETED ,tag==1 :VISETED
+	 * @param ver
 	 */
+	public void DFSRec(node_data vert , graph g) {
+		vert.setTag(1);
+		Collection<edge_data> edgesOfVert= g.getE(vert.getKey());
+		Iterator<edge_data> edge=edgesOfVert.iterator();
+		while(edge.hasNext()) {
+			node_data tempVert=g.getNode(edge.next().getDest());
+			//the next node to check is the vertex that is near we know it by the DEST
+			if(tempVert.getTag()==0)
+				DFSRec(tempVert,g);
+		}
+	}//end DFSRec
+
+	/**
+	 * return the transpose graph the this graph
+	 * @return
+	 */
+	public graph transpozeGraph () {
+		graph grafAlgoT=new DGraph();
+		Collection<node_data> vertices= this.grafAlgo.getV();
+		Iterator<node_data> vert=vertices.iterator();
+		while(vert.hasNext()) {
+			node_data tempVertex=new NodeData(vert.next().getKey());
+			tempVertex.setTag(0);//sets all the tags to 0 so can run again on the transpose graph
+			grafAlgoT.addNode(tempVertex);	
+		}	//create the Vertex, for dint stop at NULLPOINTEREXPTION with the oppisid srd and dest
+
+		Iterator<node_data> vert1=vertices.iterator();
+		while(vert1.hasNext()) {
+			node_data tempVertex=new NodeData(vert1.next().getKey());
+			//create the edges of the every vertex
+			Collection<edge_data> edgesOfVert= this.grafAlgo.getE(tempVertex.getKey());
+			Iterator<edge_data> edge=edgesOfVert.iterator();
+			while(edge.hasNext()) {
+				edge_data tempEdge=edge.next();
+				grafAlgoT.connect(tempEdge.getDest(),tempEdge.getSrc(), tempEdge.getWeight());			
+			}		
+		}//create all the transpose graph
+		return grafAlgoT;
+	}
+	
 	//****************** Private Methods and Data *****************
 	private graph grafAlgo; 
 
