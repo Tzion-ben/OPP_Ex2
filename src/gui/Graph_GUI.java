b670d2783c08259af3568ca73295210ec72ec57b
Tzion Beniaminov <tzion14@gmail.com>
2020-01-03 02:47:36 +0200

    starting the GUI again because i moved from Jframe to STDDRAW

--- src/gui/Graph_GUI.java
+++ src/gui/Graph_GUI.java
@@ -6,6 +6,8 @@ package gui;
  */
 
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
@@ -15,84 +17,67 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Collection;
 import java.util.Iterator;
-
 import javax.swing.JFrame;
-
 import algorithms.Graph_Algo;
 import dataStructure.*;
 import utils.*;
 
-public class Graph_GUI extends JFrame implements ActionListener, MouseListener
+public class Graph_GUI 
 {
 
-	/**
-	 * this method initials a graph algo from regular DGraph
-	 * @param g
-	 */
-	public void initToAlgoGraph(graph g) {
-		Graph_Algo gAlgo=new Graph_Algo(g);
-	}
+	private void initGUIGraph(graph g) 
+	{
+		Range rx=new Range(-150, 150);
+		Range ry=new Range(-150, 150);
+
 		
+		Collection<node_data> vertices= g.getV();
+		Iterator<node_data> vert=vertices.iterator();
+		if(vertices.size()!=0) {
+			StdDraw.setCanvasSize(1000,1000);
+			StdDraw.setXscale(rx.get_min(), rx.get_max());
+			StdDraw.setYscale(ry.get_min(), ry.get_max());
+			while(vert.hasNext()) {
+				node_data tempVert=vert.next();
+				drawNode(tempVert);
+			}
+		}
+		else
+			drawEmptyG();
+	}
+
+	/**
+	 * this method draw a just canvas because the graph is empty
+	 */
+	private void drawEmptyG() {
+		StdDraw.setCanvasSize(500,500);
+	}
+
+
+	public void drawNode(node_data n) {
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.setFont(new Font("TimesRoman", Font.BOLD, 15));
+		Point3D nodeLoc=n.getLocation();
+		StdDraw.point(nodeLoc.x(), nodeLoc.y());
+	}
+
+
+
+	//********************* Constructor **********************
 	/**
 	 * constructor to draw the graph
 	 */
 	public Graph_GUI(graph g) {
-		initGUIGraph();
-	}
-	
-	private void initGUIGraph() 
-	{
-		this.setSize(500, 500);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		MenuBar menuBar = new MenuBar();
-		Menu menu = new Menu("Menu Of Actions");
-		menuBar.add(menu);
-		this.setMenuBar(menuBar);
-		MenuItem sa = new MenuItem("Save");
-		MenuItem load = new MenuItem("Load");
-		MenuItem Algorithms = new MenuItem("Algorithms");
-		sa.addActionListener(this);
-		load.addActionListener(this);
-		Algorithms.addActionListener(this);
-		menu.add(sa);
-		menu.add(load);
-		menu.add(Algorithms);
-		this.addMouseListener(this);
+		initGUIGraph(g);
 	}
 
-
-	public void drawNode() {
-
+	/**
+	 * this method initials a graph algo from regular graph
+	 * @param g
+	 */
+	private void initToAlgoGraph(graph g) {
+		Graph_Algo gAlgo=new Graph_Algo(g);
 	}
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
 
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
 }
